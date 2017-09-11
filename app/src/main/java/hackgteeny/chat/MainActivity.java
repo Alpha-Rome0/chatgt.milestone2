9135ee37588c020578eeec4c5a18aa682a59780c
Mete0 <majitsu@gmail.com>
2017-09-11 14:38:30 -0400

    firebase added

--- app/src/main/java/hackgteeny/chat/MainActivity.java
+++ app/src/main/java/hackgteeny/chat/MainActivity.java
@@ -2,33 +2,71 @@ package hackgteeny.chat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-
+    String TAG="gtchat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ListView listView1 = (ListView) findViewById(R.id.listView1);
-        final EditText editText1=(EditText) findViewById(R.id.editText1);
-        ArrayList<String> chatLines = new ArrayList<>();
+        final EditText editText1 = (EditText) findViewById(R.id.editText1);
+        final ArrayList<String> chatLines = new ArrayList<>();
         final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, chatLines);
         listView1.setAdapter(listAdapter);
-        Button button= (Button) findViewById(R.id.button1);
+        Button button = (Button) findViewById(R.id.button1);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("chat");
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 listAdapter.add(editText1.getText().toString());
                 editText1.setText("");
-                listView1.setSelection(listAdapter.getCount() - 1);
+                myRef.setValue(chatLines);
             }
         });
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                GenericTypeIndicator<ArrayList<String>> t = new GenericTypeIndicator<ArrayList<String>>() {};
+                ArrayList<String> arr = dataSnapshot.getValue(t);
+                if(arr!=null){
+                    listAdapter.clear();
+                    for(String s:arr)listAdapter.add(s);
+                    listAdapter.notifyDataSetChanged();
+                    listView1.setSelection(listAdapter.getCount() - 1);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+
     }
 }
