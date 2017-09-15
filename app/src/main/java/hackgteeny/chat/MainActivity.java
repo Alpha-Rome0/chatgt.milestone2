a33bc945d52f505e11a4feddd7b071972b449451
Mete0 <majitsu@gmail.com>
2017-09-14 20:47:36 -0400

    using push and childeventlistener

--- app/src/main/java/hackgteeny/chat/MainActivity.java
+++ app/src/main/java/hackgteeny/chat/MainActivity.java
@@ -9,17 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-    String TAG="gtchat";
+    String TAG = "gtchat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,34 +36,38 @@ public class MainActivity extends Activity {
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                listAdapter.add(editText1.getText().toString());
+                myRef.push().setValue(editText1.getText().toString());
                 editText1.setText("");
-                myRef.setValue(chatLines);
             }
         });
 
         // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        ChildEventListener childEventListener = new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                GenericTypeIndicator<ArrayList<String>> t = new GenericTypeIndicator<ArrayList<String>>() {};
-                ArrayList<String> arr = dataSnapshot.getValue(t);
-                if(arr!=null){
-                    listAdapter.clear();
-                    for(String s:arr)listAdapter.add(s);
-                    listView1.setSelection(listAdapter.getCount() - 1);
-                }
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d("CHAT",dataSnapshot.getValue(String.class));
+                listAdapter.add(dataSnapshot.getValue(String.class));
+                listView1.setSelection(listAdapter.getCount() - 1);
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
             }
-        });
 
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+            }
+        };
+        myRef.addChildEventListener(childEventListener);
 
 
     }
